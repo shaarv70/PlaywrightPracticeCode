c55e57c4f8b06040c26abf2dd889dfff1e0e3e1c
Arvind <arvindsharma50480@gmail.com>
2025-04-01 02:24:05 +0530

    22nd commit

--- tests/Day-15/EncryptionDecryption.spec.ts
+++ tests/Day-15/EncryptionDecryption.spec.ts
@@ -29,7 +29,10 @@ test('Successful Login', { tag: "@Regression" }, async ({ page }) => {
     await page.goto('https://www.saucedemo.com/');
     await page.locator('[data-test="username"]').fill(decryptUsername);
     await page.locator('[data-test="password"]').fill(decryptPassword);
-    await page.locator('[data-test="login-button"]').click();
+    await page.locator('[data-test="login-button"]').waitFor({state:"visible"}).then(()=>{
+        page.locator('[data-test="login-button"]').click();
+
+    });
     await expect(page.locator('[data-test="shopping-cart-link"]')).toBeVisible();
 })
 
@@ -41,7 +44,10 @@ test('Second way', { tag: "@Smoke" }, async ({ page }) => {
     await page.goto('https://www.saucedemo.com/');
     await page.locator('[data-test="username"]').fill(decryptUsername);
     await page.locator('[data-test="password"]').fill(decryptPassword);
-    await page.locator('[data-test="login-button"]').click();
+    await page.locator('[data-test="login-button"]').waitFor({state:"visible"}).then(()=>{
+        page.locator('[data-test="login-button"]').click();
+
+    });
     await expect(page.locator('[data-test="shopping-cart-link"]')).toBeVisible();
 
 
